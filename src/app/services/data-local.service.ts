541bb34208b80a965b750545a4cb03a4e386d8c9
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-05-12 07:52:33 -0500

    local storage not duplicated movies

--- src/app/services/data-local.service.ts
+++ src/app/services/data-local.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage-angular';
 import { Movie } from '../interfaces/interfaces';
+import { ToastController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataLocalService {
 
-  constructor( private storage: Storage   ) {
+  constructor( private storage: Storage, public toastController: ToastController   ) {
     this.initDB();
   }
 
@@ -21,8 +22,34 @@ export class DataLocalService {
   }
 
   saveMovie(movie: Movie){
-    this.movies.push(movie);
+    let exist = false;
+    let mensaje = '';
+
+    for( let peli of this.movies ){
+      if( peli.id == movie.id ){
+        exist = true;
+        break;
+      }
+    }
+
+    if( exist ){
+      this.movies = this.movies.filter( mov => mov.id !== movie.id );
+      mensaje = 'Removido de favoritos';
+    }else{
+      this.movies.push(movie);
+      mensaje = 'Agregada a favoritos';
+    }
+
+    this.presentToast(mensaje);
     this.storage.set('movies', this.movies);
+  }
+
+  async presentToast(mensaje:string) {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      duration: 2000
+    });
+    toast.present();
   }
 
 
